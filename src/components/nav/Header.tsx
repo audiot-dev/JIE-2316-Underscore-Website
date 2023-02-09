826be9e548cc9a25105feb7580f1c7d1c0fe21df
thejulia1 <90807530+thejulia1@users.noreply.github.com>
2023-02-09 18:53:00 -0500

    Revert "Header/Technologytab"
    This reverts commit c1a90168866c3f114042c36d2a3100b7fb220415.

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,64 +1,21 @@
-import { AppBar, Box, Grid, Toolbar, Typography, IconButton, useMediaQuery,
-  Drawer, } from "@mui/material";
 import Link from "next/link";
-
-
 
 export default function Header() {
 
-  return (
-    
-    <Box mb={3}>
-      <AppBar position="static" sx={{backgroundColor: "secondaryBg.header"}}>
-          <Toolbar>
-          <Grid container spacing={0} alignItems="start" direction="row">
-              <Grid item xs={3}>
-                <Typography variant="h4" color="orange">
-                    <Link href="/" >audioT</Link>
-                      
-                  </Typography>
-                
-              </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
-                <Typography variant="body1" color="text.primary">
-                      <Link href="/our-mission"> Our Mission</Link>
-                    </Typography>
-                </Box>
-                
-                 
-                
-              </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
-                  <Typography variant="body1" color="text.primary">
-                    <Link href="/">Technology</Link>
-                  </Typography>
-                </Box>
-
-              </Grid>
-              <Grid item xs={2}>
-              <Box  sx={{ width:'50', height:'3', p: 2 }} borderRadius={3}>
-                  <Typography variant="body1" color="text.primary">
-                    <Link href="/about-us">About Us</Link>
-                  </Typography>
-                </Box>
-                
-              </Grid>
-              <Grid item xs={1}>
-                <Box  sx={{ width:'50', height:'3', p: 2, backgroundColor: 'orange' }} borderRadius={3}>
-                  <Typography  color="text.secondary">
-                     <Link href="/contact-us"> Contact Us</Link>
-                  </Typography>
-                    
-                </Box>
-              </Grid>
-
-            </Grid>
-          </Toolbar>
-      </AppBar>
-      
-  </Box>
-  )
+    return (
+        <div style={{display: "flex", justifyContent: "space-around"}}>
+            <Link href="/">
+                Home
+            </Link>
+            <Link href="/about-us">
+                About Us
+            </Link>
+            <Link href="/our-mission">
+                Our Mission
+            </Link>
+            <Link href="/contact-us">
+                Contact Us
+            </Link>
+        </div>
+    )
 }
-
