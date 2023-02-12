8a200889c6bf4e4db780728924cbfcc75361f6d6
Manuel Roglan <manueljoseph113@gmail.com>
2023-02-12 14:42:00 -0500

    small screen

--- src/components/home/FirstBanner.tsx
+++ src/components/home/FirstBanner.tsx
@@ -1,7 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { RedOrangePrimaryButton } from "../misc/buttons";
 
 export default function FirstBanner() {
+
+    const theme = useTheme()
+    const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
 
     return (
         <Box sx={{
@@ -15,7 +18,7 @@ export default function FirstBanner() {
             <Box bgcolor="rgba(255,255,255,.85)" px={10} py={3} mt="270px"
                 position="absolute" width="100%">
                 <Box textAlign="center" maxWidth={1400} mx="auto">
-                    <Typography variant="h3" color="secondary.main">
+                    <Typography variant={smallScreen ? 'h5' : 'h3'} color="secondary.main">
                         Audio-based welfare monitoring systems will revolutionize
                         the poultry industry.
                     </Typography>
