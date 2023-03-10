abb53ce2196de38794d82840d8b53a4bcbe84631
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2023-03-10 14:59:51 -0500

    Added a simple layout to success screen

--- src/pages/contact-us/success.tsx
+++ src/pages/contact-us/success.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Header from "../../components/nav/Header";
+import { Box, Paper, useTheme, useMediaQuery } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 export default function ContactUsSuccess() {
 
@@ -7,9 +10,32 @@ export default function ContactUsSuccess() {
             <Head>
                 <title>Contact Us | AudioT</title>     
             </Head> 
-            <div>
-                Form submitted successfully.
-            </div>
+            <Header />
+            <Box sx={{
+                backgroundColor: 'background.default',
+                width: '100vw',
+                height: '94vh',
+                py: 10
+            }}>
+                <Paper sx={{ 
+                    backgroundColor: 'background.paper',
+                    elevation: 5,
+                    mx: 'auto',
+                    maxWidth: 'sm',
+                    borderRadius: '2vh',
+                    overflow: 'hidden',
+                    textAlign: 'center'
+                }}>
+                    <Box sx={{ width: 'sm', height: 'sm', py: 5 }}>
+                        <Typography fontSize="28px" pb="40px">
+                            Form submitted successfully!
+                        </Typography>
+                        <img 
+                        style={{ aspectRatio: '1/1', width: 100, height: 100 }}
+                        src="/audiotlogo.png" />
+                    </Box>
+                </Paper>
+            </Box>
         </>
     )
 }
